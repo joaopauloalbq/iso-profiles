0c705829907c73a4a32d8fb037567758057e736c
João Paulo Albuquerque <joaopauloalbu@gmail.com>
2023-11-06 17:38:00 -0300

    Add git alias to fish

--- community/awesome/desktop-overlay/etc/skel/.config/fish/config.fish
+++ community/awesome/desktop-overlay/etc/skel/.config/fish/config.fish
@@ -36,5 +36,25 @@ abbr -a ps pamac search
 abbr -a pu pamac update
 abbr -a pc pamac checkupdates
 abbr -a pd pamac info
-abbr -a ran ranger
+abbr -a gad git add
+abbr -a gbr git branch
+abbr -a gco git commit
+abbr -a gcl git clone
+abbr -a gck git checkout
+abbr -a gcp git cherry-pick 
+abbr -a gdi git diff
+abbr -a gfe git fetch
+abbr -a gin git init
+abbr -a glo git log --graph --abbrev-commit --pretty=oneline
+abbr -a gme git merge
+abbr -a grb git rebase
+abbr -a grt git restore
+abbr -a grm git remote
+abbr -a gst git status
+abbr -a gsw git switch
+abbr -a gpu git pull
+abbr -a gpx git push
+abbr -a gt git tag
+
+abbr -a ra ranger
 abbr -a laz lazygit
