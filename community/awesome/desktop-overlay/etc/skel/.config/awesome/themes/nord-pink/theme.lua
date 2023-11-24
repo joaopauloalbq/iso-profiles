65ffeb606f43feff1fcee7356275cb918757c0c3
João Paulo Albuquerque <joaopauloalbu@gmail.com>
2023-11-24 11:34:15 -0300

    Update theme.lua

--- community/awesome/desktop-overlay/etc/skel/.config/awesome/themes/nord-pink/theme.lua
+++ community/awesome/desktop-overlay/etc/skel/.config/awesome/themes/nord-pink/theme.lua
@@ -2,10 +2,11 @@ local theme_assets = require("beautiful.theme_assets")
 local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
 
+-- local awful = require("awful")
 local gears = require("gears")
-local lain  = require("lain")
 local gfs = require("gears.filesystem")
 local themes_path = gfs.get_themes_dir()
+local wibox = require("wibox")
 local theme = {}
 
 theme.font          = "Noto Sans Medium 11"
@@ -49,12 +50,13 @@ theme.notification_margin = dpi(6)
 -- taglist_[bg|fg]_[focus|urgent|occupied|empty|volatile]
 -- tasklist_[bg|fg]_[focus|urgent]
 -- titlebar_[bg|fg]_[normal|focus]
--- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
+-- tooltip_[font|opacity|fg|bg|border_width|border_color]
 -- mouse_finder_[color|timeout|animate_timeout|radius|factor]
 -- prompt_[fg|bg|fg_cursor|bg_cursor|font]
 -- hotkeys_[bg|fg|border_width|border_color|shape|opacity|modifiers_fg|label_bg|label_fg|group_margin|font|description_font]
 -- Example:
 --theme.taglist_bg_focus = "#ff0000"
+theme.tooltip_bg = "#131519"
 
 theme.titlebar_bg_normal = "#1e222a" 
 theme.titlebar_bg_focus = theme.titlebar_bg_normal
@@ -138,6 +140,8 @@ theme.titlebar_floating_button_normal_active = "~/.config/awesome/themes/nord-pi
 theme.titlebar_floating_button_focus_active  = "~/.config/awesome/themes/nord-pink/titlebar/sepa.png"
 
 theme.wallpaper = "~/.wallpaper"
+
+require("awful").spawn.easy_async('xdg-user-dir PICTURES', function(dir) theme.picture_dir = dir:sub(1,-2) end)
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "~/.config/awesome/themes/nord-pink/layouts/fairhw.png"
